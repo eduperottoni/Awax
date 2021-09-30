99d587a469fc89277a8d69005fdcf734ab6c64de
eduperottoni <edu.perottoni@gmail.com>
2021-09-30 00:22:42 -0300

    Finishing Carroussel feature

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,23 +1,47 @@
-const slider = document.querySelector(".home-slider");
-const slides = Array.from(document.getElementsByClassName("home-slider-slide"));
-const points = Array.from(
-  document.getElementsByClassName("home-pointer-point")
-);
+const slider = document.querySelector(".home-slider")
+const points = Array.from(document.getElementsByClassName('home-pointer-point'))
+const slidesSpan = Array.from(document.querySelectorAll('.home-slider-slide-area span'))
+console.log(points)
 
-console.log(slider, slides, points);
-
-function defineArrayIndexes(array) {
-  array.forEach((item) => {
-    item.setAttribute('value', array.indexOf(item));
-    item.setAttribute('onclick','activePointer(value)')
-  });
+function changeSlideAuto(){
+  setInterval(()=>{
+    let spanActive = document.querySelector('div.home-pointer-point.active')
+    let activePosition = points.indexOf(spanActive)
+    activePosition == points.length-1 ? activePosition = -1 : 
+    console.log(activePosition)
+    removeActiveClass()
+    addActiveClass(activePosition+1)
+    changeSliderMargin(activePosition+1)
+  }, 6000)
 }
 
-defineArrayIndexes(slides);
-defineArrayIndexes(points);
+changeSlideAuto()
 
+function changeSlide(value) {
+  changeSliderMargin(value)
+  removeActiveClass()
+  addActiveClass(value)
+}
 
-function activePointer(value) {
-    console.log(value)
+function changeSliderMargin(value){
+  let sliderMarginValue = value * 100;
+  let sliderMarginTxt = `-${sliderMarginValue}vw`
+  slider.style.marginLeft = sliderMarginTxt
+}
+
+function removeActiveClass(){
+  //removing 'active' class of all points and spans
+  points.forEach((item) =>{
+    item.classList.remove('active')
+  })
+  slidesSpan.forEach((item)=>{
+    item.classList.remove('active')
+  })
+}
+
+function addActiveClass(index){
+  //adding 'active' class to the clicked point and span
+  points[index].classList.add('active')
+  slidesSpan[index].classList.add('active')
 }
 
